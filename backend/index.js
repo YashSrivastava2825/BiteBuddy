f061221b9965be8f1c89fae6a891d820dcd86571
Yash Srivastava <112803166+YashSrivastava2825@users.noreply.github.com>
2023-09-21 12:27:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 5000
 const mongoDB = require('./db');
 mongoDB();
+
+app.use(cors());
 app.use((req, res, next) =>{
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
